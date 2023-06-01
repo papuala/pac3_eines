5aae1db908c3bbc81da467b043b6a54b1c577ae5
papuala <pauladiazcarreras1b@gmail.com>
2023-06-01 20:00:57 +0200

    footer and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      colors: {
+        'dark': '#7b5e22',
+        'light': '#f1dcbf',
+      },
       width: {
         '350': '350px',
         '400': '400px',
