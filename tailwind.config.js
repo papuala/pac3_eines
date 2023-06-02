303bc4a991545760bc5a4d775bdbd7aed72685e9
papuala <pauladiazcarreras1b@gmail.com>
2023-06-02 17:24:21 +0200

    posthtml-include

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,35 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark': '#7b5e22',
-        'light': '#f1dcbf',
+        'brown-dark': '#7b5e22',
+        'brown-light': '#f1dcbf',
+        'brown-lighter': '#f9f1e5',
       },
       width: {
+        '50': '50px',
+        '60': '60px',
+        '70': '70px',
         '350': '350px',
         '400': '400px',
+      },
+      height: {
+        '50': '50px',
+        '60': '60px',
+        '70': '70px',
+        '100':'100px',
+        '250': '250px',
+        '50vh': '50vh',
+      },
+      maxWidth: {
+        '4/5': '80%',
+      },
+      minHeight: {
+        '100':'100px',
+      },
+      spacing: {
+        '1/10': '10%',
+        '50': '50px',
+        '100': '100px',
       }
     },
   },
